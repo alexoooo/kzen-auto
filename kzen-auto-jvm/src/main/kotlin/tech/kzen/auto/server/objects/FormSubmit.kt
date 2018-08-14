25a771804d4f0767a31f51842915b204f5ecfae6
alexoooo <alex@ostrovsky.biz>
2018-08-14 08:38:26 -0400

    added FormSubmit / LeftClick / SendKeys

---
+++ kzen-auto-jvm/src/main/kotlin/tech/kzen/auto/server/objects/FormSubmit.kt
@@ -0,0 +1,20 @@
+package tech.kzen.auto.server.objects
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+import tech.kzen.auto.common.api.AutoAction
+import tech.kzen.auto.server.service.ServerContext
+
+
+class FormSubmit(
+        var xpath: String
+) : AutoAction {
+    override fun perform() {
+        val driver = ServerContext.webDriverContext.get()
+
+        val element: WebElement =
+                driver.findElement(By.xpath(xpath))
+
+        element.submit()
+    }
+}
---
+++ kzen-auto-jvm/src/main/kotlin/tech/kzen/auto/server/objects/SendKeys.kt
@@ -0,0 +1,23 @@
+package tech.kzen.auto.server.objects
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+import tech.kzen.auto.common.api.AutoAction
+import tech.kzen.auto.server.service.ServerContext
+
+
+class SendKeys(
+        var xpath: String,
+        var text: String
+) : AutoAction {
+    override fun perform() {
+        // https://stackoverflow.com/questions/44455269/gmail-login-using-selenium-webdriver-in-java
+
+        val driver = ServerContext.webDriverContext.get()
+
+        val element: WebElement =
+                driver.findElement(By.xpath(xpath))
+
+        element.sendKeys(text)
+    }
+}
