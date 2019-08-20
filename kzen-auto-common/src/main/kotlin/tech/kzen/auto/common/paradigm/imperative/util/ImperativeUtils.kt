f670d84d36cc4760c3846b48f326758029897f0b
alexoooo <ostrovsky.alex@gmail.com>
2019-08-20 19:28:25 -0400

    ImperativeUtils next-to-run improvements

--- kzen-auto-common/src/main/kotlin/tech/kzen/auto/common/paradigm/imperative/util/ImperativeUtils.kt
+++ kzen-auto-common/src/main/kotlin/tech/kzen/auto/common/paradigm/imperative/util/ImperativeUtils.kt
@@ -1,8 +1,9 @@
 package tech.kzen.auto.common.paradigm.imperative.util
 
+import tech.kzen.auto.common.paradigm.imperative.model.ImperativeFrame
 import tech.kzen.auto.common.paradigm.imperative.model.ImperativeModel
 import tech.kzen.auto.common.paradigm.imperative.model.ImperativePhase
-import tech.kzen.auto.common.paradigm.imperative.model.control.ControlTree
+import tech.kzen.auto.common.paradigm.imperative.model.control.*
 import tech.kzen.lib.common.model.locate.ObjectLocation
 import tech.kzen.lib.common.structure.GraphStructure
 
@@ -26,16 +27,59 @@ object ImperativeUtils {
 
         val controlTree = ControlTree.readSteps(graphStructure, lastFrame.path)
 
-        var pending: ObjectLocation? = null
+        return nextInBranch(controlTree, lastFrame)
 
-        controlTree.traverseDepthFirst { stepLocation ->
-            val objectPath = stepLocation.objectPath
+//        var pending: ObjectLocation? = null
+//
+//        controlTree.traverseDepthFirst { stepLocation ->
+//            val objectPath = stepLocation.objectPath
+//
+//            if (lastFrame.states[objectPath]?.phase() == ImperativePhase.Pending) {
+//                pending = stepLocation
+//            }
+//        }
+//
+//        return pending
+    }
 
-            if (lastFrame.states[objectPath]?.phase() == ImperativePhase.Pending) {
-                pending = stepLocation
+
+    private fun nextInBranch(
+            branchControlNode: BranchControlNode,
+            imperativeFrame: ImperativeFrame
+    ): ObjectLocation? {
+        for (node in branchControlNode.nodes) {
+            val state = imperativeFrame.states[node.step.objectPath]
+                    ?: continue
+
+            if (state.previous != null) {
+                continue
+            }
+
+            val isNext = when (
+                val controlState = state.controlState
+            ) {
+                null -> true
+                InitialControlState -> true
+                FinalControlState -> true
+
+                is BranchEvaluationState -> {
+                    val branch = node.branches[controlState.index]
+
+                    val nextInBranch = nextInBranch(branch, imperativeFrame)
+
+                    if (nextInBranch != null) {
+                        return nextInBranch
+                    }
+
+                    true
+                }
+            }
+
+            if (isNext) {
+                return node.step
             }
         }
 
-        return pending
+        return null
     }
 }
