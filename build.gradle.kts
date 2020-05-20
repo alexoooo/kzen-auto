9577d8fbf2c71b1b65d3b396a7d545fc7d312339
alexoooo <alex@ostrovsky.biz>
2020-05-20 11:43:47 -0400

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.auto"
-    version = "0.14.1"
+    version = "0.15.0-SNAPSHOT"
 
     repositories {
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
