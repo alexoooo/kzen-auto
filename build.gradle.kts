24f9f932a367fca35ce8341d9ef3e4c740286542
alexoooo <alex@ostrovsky.biz>
2020-12-02 10:27:05 -0500

    release 0.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.auto"
-    version = "0.19.0-SNAPSHOT"
+    version = "0.19.0"
 
     repositories {
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
