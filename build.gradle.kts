b190c6ed42a423c5d4f86bbf8d0c47bdb58b59ec
alexoooo <alex@ostrovsky.biz>
2026-05-09 23:35:44 -0400

    version bump | kotlin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.auto"
-    version = "0.29.0-SNAPSHOT"
+    version = "0.29.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 
-const val kotlinVersion = "2.3.0"
+const val kotlinVersion = "2.3.21"
 const val jvmTargetVersion = "25"
 const val javaVersion = 25
 const val jvmToolchainVersion = 25
