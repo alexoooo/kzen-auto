6f4949bfc078c1c4fe9959d97da7e86adab91d7e
alexoooo <alex@ostrovsky.biz>
2026-05-10 13:47:17 -0400

    server-side dependency updates

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,13 +5,13 @@ const val javaVersion = 25
 const val jvmToolchainVersion = 25
 
 
-const val kzenLibVersion = "0.29.0-SNAPSHOT"
+const val kzenLibVersion = "0.29.1-SNAPSHOT"
 
 
-const val ktorVersion = "3.3.3"
+const val ktorVersion = "3.4.3"
 const val annotationsApiVersion = "3.0.0"
-const val logbackVersion = "1.5.23"
-const val jacksonModuleKotlin = "3.0.3"
+const val logbackVersion = "1.5.32"
+const val jacksonModuleKotlin = "3.1.3"
 const val commonsCompressVersion = "1.28.0"
 const val fastutilVersion = "8.5.18"
 const val bifurcanVersion = "0.2.0-rc1"
@@ -19,14 +19,14 @@ const val h2Version = "2.4.240"
 const val zeroAllocationHashingVersion = "0.27ea1"
 const val disruptorVersion = "4.0.0"
 const val bloomFilterVersion = "0.9.0"
-const val commonsIoVersion = "2.21.0"
+const val commonsIoVersion = "2.22.0"
 const val migzVersion = "2.0.beta-1"
 
-const val seleniumVersion = "4.39.0"
-const val webdrivermanagerVersion = "6.3.3"
+const val seleniumVersion = "4.41.0"
+const val webdrivermanagerVersion = "6.3.4"
 
 
-const val kotlinxDatetimeVersion = "0.7.1"
+const val kotlinxDatetimeVersion = "0.8.0"
 const val kotlinxHtmlVersion = "0.12.0"
 const val lodashVersion = "4.17.21"
 const val cropperJsVersion = "2.1.0"
